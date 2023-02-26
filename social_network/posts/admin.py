2bcc4514aea5728cef91d20d42ed35b4c7dc0ba9
michael-babuhin <graf02030405@yandex.ru>
2023-02-26 20:04:15 +0700

    Add admin setting and base post's view

--- social_network/posts/admin.py
+++ social_network/posts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    #Перечесление полей пост
+    list_display = ("text", "pub_date", "author")
+    #Добавляем интерфейс для поиска по тексту постов
+    search_fields = ("text",)
+    #Фильтрация по дате
+    list_filter = ("pub_date", )
+# при регистрации модели Post источником конфигурации для неё назначаем класс PostAdmin
+admin.site.register(Post, PostAdmin)
--- social_network/posts/views.py
+++ social_network/posts/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Post
+
+def index(request):
+    #Запрос к базе постов
+    latest = Post.objects.order_by("-pub_date")[:10]
+    output = []
+    #Собираем тексты постов в один, разделяя новой строкой
+    for item in latest:
+        output.append(item.text)
+    
+    return HttpResponse('\n'.join(output))
